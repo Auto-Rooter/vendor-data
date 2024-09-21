88e7630a4f6fb07818607444ff6c0b16a0a86415
Hadi Assalem <hadiassalem@cloud.upwork.com>
2024-09-21 18:15:02 +0200

    Feat(AWS): Seed the Database with dummy data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { dynamodbCreateTable, dynamodbDescribeTable, dynamodbDeleteTable } from "./aws";
+import { dynamodbCreateTable, dynamodbDescribeTable, dynamodbDeleteTable, dynamodbCreateRecord } from "./aws";
+import vendors from "./data/vendors";
+
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 const init = async() => {
     const vendorTableName = 'vendors';
@@ -7,23 +10,32 @@ const init = async() => {
     if(!(vendorsTable instanceof Error)){
         console.log("Table Exists");
         await dynamodbDeleteTable(vendorTableName);
+        await delay(6000);
     }
-    // const vendorsTableParams: AWS.DynamoDB.CreateTableInput = {
-    //     TableName: 'vendors',
-    //     KeySchema: [
-    //         { AttributeName: 'twitterId', KeyType: 'HASH'}
-    //     ],
-    //     AttributeDefinitions: [
-    //         { AttributeName: 'twitterId', AttributeType: 'S' }
-    //     ],
-    //     ProvisionedThroughput: {
-    //         ReadCapacityUnits: 10,
-    //         WriteCapacityUnits: 10
-    //     }
-    // };
+    const vendorsTableParams: AWS.DynamoDB.CreateTableInput = {
+        TableName: 'vendors',
+        KeySchema: [
+            { AttributeName: 'twitterId', KeyType: 'HASH'}
+        ],
+        AttributeDefinitions: [
+            { AttributeName: 'twitterId', AttributeType: 'S' }
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 10,
+            WriteCapacityUnits: 10
+        }
+    };
 
-    // dynamodbCreateTable(vendorsTableParams);
+    await dynamodbCreateTable(vendorsTableParams);
+    await delay(6000);
 
+
+    for(const i in vendors){
+        const res = await dynamodbCreateRecord(vendorTableName, vendors[i]);
+        if(res instanceof Error){
+            console.log("[!] Error: " + res + JSON.stringify(vendors[i]));
+        }
+    }
 }
 
 init();
